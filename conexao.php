69ee33e18d2f9e9faf6f4a3b75408ad07fa338e7
LucasRalf14 <lucas_w.o.ralf@hotmail.com>
2021-05-29 02:22:52 -0300

    modificado inicialmente mas, voltou ao código original

--- conexao.php
+++ conexao.php
@@ -6,6 +6,7 @@ date_default_timezone_set('America/Recife');
 
 try {
     $pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+
 } catch (Exception $e){
     echo "Erro ao conectar com o banco de dados! " . $e;
 }
